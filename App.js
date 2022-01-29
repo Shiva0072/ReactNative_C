0b306ee3965c2c6d62c97cc691aca08517ea3b72
Shivam <devshiva.iit.kgp@gmail.com>
2022-01-29 18:06:57 +0530

    layout

--- App.js
+++ App.js
@@ -1,42 +1,6 @@
-import {
-  StyleSheet,
-  Text,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableHighlight,
-  SafeAreaView,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-} from "react-native";
+import ViewScreen from "./app/screens/ViewScreen";
+import WelcomeScreen from "./app/screens/WelcomScreen";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Startewe Native</Text>
-      <TouchableHighlight onPress={() => console.log("pressedMe")}>
-        <Image
-          source={{ uri: "https://picsum.photos/200/300" }}
-          style={{ width: 190, height: 150 }}
-          blurRadius={1}
-        />
-      </TouchableHighlight>
-
-      <Button
-        title="Click Me"
-        onPress={() => Alert.alert("Alert", "You clicked Me")}
-      />
-    </SafeAreaView>
-  );
+  return <ViewScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "pink",
-    // alignItems: "center",
-    // justifyContent: "center",
-    padding: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
